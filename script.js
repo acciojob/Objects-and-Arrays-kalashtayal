15a15d80cc2027031aea716bfb89aa916f0b7512
kalash tayal <130529290+kalashtayal@users.noreply.github.com>
2023-05-19 12:17:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,14 +12,7 @@ const person = {
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 var team = players;
-var team1 = "";
-	for (let i = 0; i < players.length; i++) { 
-		team1[i]=players[i];
-	 }
-var cap1 = person();
+var team1 = Array.from(players);
+	
+var cap1 = {...person};
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
